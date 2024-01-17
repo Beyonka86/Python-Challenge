aa44aaacbeb92024b7689fe95f791ce1d5432bde
Beyonka Powell <beyonka86@gmail.com>
2024-01-17 17:26:13 -0600

    finished Pybank assignment

--- PyBank/main.py
+++ PyBank/main.py
@@ -6,14 +6,14 @@ budget_data_out = os.path.join('.', 'analysis', 'budget_analysis.txt')
 
 
 #init variables
-total_months = 0
+total_months = 1
 revenue_total = 0
 revenue = []
 previous_revenue = 0
 revenue_change = 0
 month_change = []
-greatest = ["", 9999999]
-least = ["", 0]
+greatest = ["", -9999999]
+least = ["", 9999999]
 revenue_list = []
 revenue_average = 0
 
@@ -24,31 +24,34 @@ with open(budget_data_csv) as in_file:
     header = next(reader)
 
     row = next(reader)
-    
+   
+    revenue_total = revenue_total + int(row[1])
+    previous_revenue = float(row[1])
+
     #Loop through rows
     for row in reader:
 
         #calculate total number of months       
-        total_months =+ total_months
+        total_months = total_months + 1
         
  #calculate total over period       
-        revenue_total = revenue_total + int(row["Profit/Losses"])
+        revenue_total = revenue_total + int(row[1])
 
  #calculate the average change       
-        revenue_change = float(row["Profit/Losses"]) - previous_revenue
-        previous_revenue = float(row["Profit/Losses"])
+        revenue_change = float(row[1]) - previous_revenue
+        previous_revenue = float(row[1])
         revenue_list = revenue_list + [revenue_change]
-        month_change = [month_change] + [row["Date"]]
+        month_change = [month_change] + [row[0]]
 
         #calculating Greatest Increase in Profits
         if revenue_change > greatest[1]:
             greatest[1] = revenue_change
-            greatest[0] = row["Date"]
+            greatest[0] = row[0]
 
         #calculating Greatest Decrease in Profits
         if revenue_change < least[1]:
             least[1] = revenue_change
-            least[0] = row["Date"]
+            least[0] = row[0]
 revenue_average = sum(revenue_list) / len(revenue_list)
 
 
@@ -59,9 +62,9 @@ output = (
     f"------------------------------------------------\n"
     f"Total Months: {total_months}\n"
     f"Total: ${revenue_total}\n"
-    f"Average Change: ${revenue_average}\n"
-    f"Greatest Increase in Profits: {greatest[0], {greatest}}\n"
-    f"Greatest Decrease in Profits: {least[0], {least[1]}}"
+    f"Average Change: ${round(revenue_average, 2)}\n"
+    f"Greatest Increase in Profits: {greatest[0]} (${int(greatest[1])})\n"
+    f"Greatest Decrease in Profits: {least[0]} (${int(least[1])})"
                                      )
 
 print(output)
@@ -69,12 +72,4 @@ with open(budget_data_out,"w") as out_file:
     out_file.write(output)
 
 
-
-# Financial Analysis
-# ----------------------------
-# Total Months: 86
-# Total: $22564198
-# Average Change: $-8311.11
-# Greatest Increase in Profits: Aug-16 ($1862002)
-# Greatest Decrease in Profits: Feb-14 ($-1825558)
 
